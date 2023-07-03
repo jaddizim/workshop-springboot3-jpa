d4242c9032fb005808ada57da66721a1f22f173d
jaddizim <jaddizim@gmail.com>
2023-07-03 14:19:47 -0300

    Substotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -32,7 +32,7 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
-    @OneToOne(mappedBy = "order",  cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
 
     public Order() {
@@ -88,6 +88,15 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Double getTotal() {
+        double sum = 0.0;
+
+        for (OrderItem item : items) {
+            sum += item.getSubTotal();
+        }
+        return sum;
     }
 
     @Override
--- src/main/java/com/educandoweb/course/entities/Payment.java
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -1,6 +1,7 @@
 package com.educandoweb.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -22,6 +23,7 @@ public class Payment implements Serializable {
 
     @OneToOne
     @MapsId
+    @JsonIgnore
     private Order order;
 
     public Payment() {
